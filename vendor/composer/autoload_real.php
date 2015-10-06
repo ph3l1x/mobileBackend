e433e50fee3f9d7aee449148954afbed08244972
ph3l1x <rperry@mindimage.net>
2015-10-05 23:59:31 -0600

    Added Authentication with Laravel http://scotch.io/tutorials/token-based-authentication-for-angularjs-and-laravel-apps

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit6b2f958dfa1c52b85973599dfddd4120
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit6b2f958dfa1c52b85973599dfddd4120', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
