f994a926184e38b7da30b83f54d3b27213da1764
ph3l1x <rperry@mindimage.net>
2015-10-05 22:31:49 -0600

    Working Talking with Script

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-fuck
+
+header('Access-Control-Allow-Origin: *');
+
+//$link = mysql_connect('localhost','root','`m1nd1m4g3`');
+//mysql_select_db('correntebellosub',$link);
+
+$params = json_decode(file_get_contents('php://input'), true);
+
+$pUsername = $params['username'];
+$pPassword = $params['password'];
+
+$username = "phelix";
+$password = "pass";
+
+
+
+$results = array('username' => $pUsername, 'password' => $pPassword);
+
+print json_encode($results);
+
+//return json_encode($results);
+?>
